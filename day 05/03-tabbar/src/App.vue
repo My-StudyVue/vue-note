33928e1d53d7f8f274c95314b539e31ad3cb315f
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2020-10-08 22:48:33 +0800

    vue笔记

--- day 05/03-tabbar/src/App.vue
+++ day 05/03-tabbar/src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div  class="tab-bar-item">首页</div>
-      <div  class="tab-bar-item">分类</div>
-      <div  class="tab-bar-item">购物车</div>
-      <div  class="tab-bar-item">我的</div>
-    </div>
+    <tab-bar>
+      <tab-bar-item>
+        <img src="./assets/img/tabbar/home.png" alt="">
+        <div>首页</div>
+      </tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+  import TabBar from './components/tabbar/TabBar'
+  import TabBarItem from './components/tabbar/TabBarItem.vue'
+
   export default {
     name: 'App',
+    components:{
+      TabBar,
+      TabBarItem
+    }
   }
 </script>
 
@@ -19,22 +29,4 @@
   /* @import './assets/css/base.css' */
   @import url('./assets/css/base.css');
 
-  #tab-bar{
-    display: flex;
-    /* justify-content: space-around; */
-    background-color: #f6f6f6;
-
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-
-    /* 阴影 */
-    box-shadow: 0 -3px 1px rgba(100, 100, 100, .8);
-  }
-  .tab-bar-item{
-    flex:1;
-    text-align: center;
-    height: 49px;/* tabbar一般高度 */
-  }
 </style>
---
+++ day 05/03-tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+    <!-- 固定 -->
+    <!-- <img src="../../assets/img/tabbar/home.png" alt="">
+    <div>首页</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name:"TabBarItem",
+  components: {},
+  data() {
+    return {
+      
+    };
+  },
+}
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    flex:1;
+    text-align: center;
+    height: 49px;/* tabbar一般高度 */
+  }
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+  }
+</style>
